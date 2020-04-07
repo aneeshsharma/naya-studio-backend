f05d313b0238a9c33766432b4c2ce3997c1e7f01
Anish Sharma <aneeshsharma15@outlook.com>
2020-04-07 15:41:30 +0530

    Implemented saving data on database using MongoDB

--- main.js
+++ main.js
@@ -3,14 +3,37 @@ var bodyParser = require("body-parser");
 var cors = require("cors");
 var app = express();
 
-app.use(bodyParser.json());
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/";
 
+app.use(bodyParser.json());
 
 app.use(cors());
 
+var _db, _dbo;
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("nayadb");
+    dbo.createCollection("users", function (err, res) {
+        if (err) throw err;
+        console.log("Collection Created");
+    });
+
+    _dbo = dbo;
+    _db = db;
+});
+
 app.get("/", (req, res) => {
     console.log(req.query);
-    res.send("FAILED");
+    _dbo.collection("users").insertOne(req.query, function (err, dbres) {
+        if (err) {
+            res.send("FAILED");
+        } else {
+            console.log("Inerted Data!");
+            res.send("SUCCESS");
+        }
+    });
 });
 
 var server = app.listen(7800, function () {
